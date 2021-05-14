4cd63862306d8b21c6a0d5fd4114c3879768d2a2
wyllisMonteiro <wyllismonteiro@gmail.com>
2021-05-14 23:35:46 +0200

    style: lint fix

--- api/__init__.py
+++ api/__init__.py
@@ -11,32 +11,31 @@ from api.v1.auth.auth import ns as auth
 app = Flask(__name__)
 logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../logging.conf'))
 logging.config.fileConfig(logging_conf_path)
-log = logging.getLogger(__name__)          
+log = logging.getLogger(__name__)
 # https://github.com/postrational/rest_api_demo
 
 def configure_app(flask_app):
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
-    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
-    flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
-    flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE
-    flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
-    flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
+  flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
+  flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
+  flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
+  flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE
+  flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
+  flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
 
 def initialize_app(flask_app):
-    configure_app(flask_app)
+  configure_app(flask_app)
 
-    blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
-    api.init_app(blueprint)
-    api.add_namespace(auth)
+  blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
+  api.init_app(blueprint)
+  api.add_namespace(auth)
 
-    flask_app.register_blueprint(blueprint)
+  flask_app.register_blueprint(blueprint)
 
-    database.init_app(flask_app)
+  database.init_app(flask_app)
 
 def main():
-    initialize_app(app)
-    log.info('>>>>> Starting development server at http://localhost:5000/api/v1/ <<<<<')
-    app.run(debug=settings.FLASK_DEBUG)
+  initialize_app(app)
+  log.info('>>>>> Starting development server at http://localhost:5000/api/v1/ <<<<<')
+  app.run(debug=settings.FLASK_DEBUG)
 
-#if __name__ == "__main__":
 main()
